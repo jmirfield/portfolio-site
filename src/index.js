826de5f3e3183a4b3bc5510be6dfd481f49905e3
jmirfield <justin.mirfield@gmail.com>
2022-05-08 15:57:15 -0400

    Moved profile image to cloudinary

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import './styles/global.css';
-import profile from './../public/assets/profile.JPG';
 import resume from './../public/assets/resume.pdf';
 import mainCanvas from './scripts/mainCanvas';
 import setHeaderEvents from './scripts/headerEvents';
 
-document.getElementById('profile').src = profile;
 document.getElementById('resume').setAttribute('href', resume);
 
 window.onload = () => {
